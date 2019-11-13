c8b6ad92ca9cb581e2e997db3388945b3778f94d
rex100h <houtanfaridi@gmail.com>
2019-11-13 02:17:02 -0600

    Added basic command line argument parsing and flags

---
+++ src/Commands/Arguments.hs
@@ -0,0 +1,55 @@
+module Commands.Arguments where 
+
+import Options.Applicative
+import Data.Semigroup ((<>))
+
+data CmdOptions = CmdOptions {
+    spattern   :: String
+  , source    :: [String]
+  , recursive :: Bool
+  , caps     :: Bool
+}
+
+
+commandParser :: ParserInfo CmdOptions
+commandParser = info ( commands <**> helper ) (
+                  fullDesc
+                  <> progDesc "Print lines that match patterns"
+                  <> header "HaskellGrep - A Haskell implementation of Grep"
+                )
+
+
+commands :: Parser CmdOptions
+commands = CmdOptions
+            <$> patternArg
+            <*> sourceArg
+            <*> recursiveFlag
+            <*> characterCaseFlag
+
+
+patternArg :: Parser String
+patternArg = argument str (
+              metavar "PATTERN"
+             )
+
+
+sourceArg :: Parser [String]
+sourceArg = many $ argument str (
+              metavar "FILES/DIRECTORIES..."
+            )
+
+
+recursiveFlag :: Parser Bool
+recursiveFlag = switch (
+                  long "recursive"
+                  <> short 'r'
+                  <> help "Recursively search files down a directory tree"
+                )
+
+
+characterCaseFlag :: Parser Bool
+characterCaseFlag = switch (
+                      long "ignore-case"
+                      <> short 'i'
+                      <> help "Ignore case when matching pattern"
+                    )
