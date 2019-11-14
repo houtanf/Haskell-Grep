3a257111479fe50be0f37db56f4d8f5932a1697f
rex100h <houtanfaridi@gmail.com>
2019-11-14 01:17:12 -0600

    added stdin reading when file is not provided

--- src/Main.hs
+++ src/Main.hs
@@ -3,11 +3,12 @@ module Main where
 import Reading.Files.GetFiles (getFiles)
 import Reading.Files.ReadFiles (fileLines)
 
-import Parsing.ReadUtils (appendName)
 import Parsing.Match (matchLines)
+import Parsing.ReadUtils (appendName)
 
 import Commands.Arguments ( commandParser, CmdOptions(..) )
 
+import System.IO
 import Options.Applicative
 import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Streaming.Prelude as S
@@ -18,12 +19,13 @@ main = parseOptions =<< execParser commandParser
 
 parseOptions :: CmdOptions -> IO ()
 parseOptions (CmdOption pattern paths recurse _)
-  | null paths = undefined
+  | null paths = do 
+                  hSetBuffering stdin LineBuffering
+                  B.interact $ B.unlines . matchLines pattern . B.lines
   | otherwise = S.mapM_ (uncurry $ evalFiles pattern) 
                 . fileLines 
                 . getFiles recurse $ paths
                 
-
 
 evalFiles :: String -> FilePath -> IO [B.ByteString] -> IO ()
 evalFiles pattern filename file = file >>= mapM_ B.putStrLn
